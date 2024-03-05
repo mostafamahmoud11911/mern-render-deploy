b4ab2d27ac2034458bf2e59bd6f7a6ff1754af2b
mostafamahmoud99 <mm0766633@gmail.com>
2024-03-05 03:48:11 +0200

    'update'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,49 @@
 import axios from "axios";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({ username: "", email: "", password: "" });
 
-  const handleSubmit =async (e)=>{
+  const getUser = ({ target }) => {
+    setUser({ ...user, [target.name]: target.value });
+  };
+
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const {data} = await axios.post(`http://localhost:5500/api/auth/register`);
-      console.log(data)
+      const { data } = await axios.post(
+        `http://localhost:5500/api/auth/register`, user
+      );
+      console.log(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    const getData =async () =>{
+  useEffect(() => {
+    const getData = async () => {
       try {
-        const {data} = await axios.get('http://localhost:5500/api/auth');
-        console.log(data)
+        const { data } = await axios.get("http://localhost:5500/api/auth");
+        console.log(data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-
-    }
-    getData()
-  },[])
+    };
+    getData();
+  }, []);
   return (
     <>
       <div className="App">
         <form onSubmit={handleSubmit}>
           <label htmlFor="">username</label>
-          <input type="text" name="username"/>
+          <input type="text" name="username" onChange={getUser} />
           <label htmlFor="">email</label>
-          <input type="text" name="email"/>
+          <input type="text" name="email" onChange={getUser} />
           <label htmlFor="">password</label>
-          <input type="password" name="password"/>
+          <input type="password" name="password" onChange={getUser} />
           <button>Create</button>
         </form>
       </div>
